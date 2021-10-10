c14f35cb3c0cbd2c5e72fee6f520cefd18d6cf68
JNM87 <J.molina22587@gmail.com>
2021-10-09 23:23:32 -0400

    react working

---
+++ src/resumeData.js
@@ -0,0 +1,71 @@
+let resumeData = {
+  "imagebaseurl":"https://Jnm8787.github.io/",
+  "name": "Jose Molina Paez",
+  "role": "aspiring Full Stack Developer",
+  "linkedinId":"Your LinkedIn Id",
+  
+  
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/jmolina87/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/Jnm8787",
+        "className":"fa fa-github"
+      },
+      
+    ],
+  "aboutme":"I am junior aspiring Full Stack Developer. I'm looking forward to start my career in the tech industry and succeed in such a growing community. I was born in Argentina and raised in the United States. I’m bilingual, I speak Spanish. My favorite things are music, sports, foodie, tech, and the most important of all is my family.",
+  
+  
+  "skillsDescription":"Languages",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"JavaScript"
+    },
+    {
+      "skillname":"ReactJS"
+    },
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"Marvel",
+      "imgurl":"images/marvel.png",
+      "url":"https://tloyzelle.github.io/Project-Marvel/"
+    },
+    {
+      "name":"project2",
+      "description":"Vibez",
+      "imgurl":"images/vibez-3.png"
+    },
+    {
+      "name":"Homework",
+      "description":"Day Planner",  
+      "imgurl":"images/d-p2.png"
+    },
+  ],
+    "testimonials":[
+      {
+        "description":"Jose is Awesome.",
+        "name":"Luisa"
+      },
+      {
+        "description":"He does great work.",
+        "name":"Marcus"
+      }
+    ]
+  
+  
+}
+
+export default resumeData
